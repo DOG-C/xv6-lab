fd0c66defb5c7d4c6ad617810e2c6f07051ce6c6
dogc <caoyuqi1996@hotmail.com>
2025-04-21 23:48:07 +0200

    my solution for util lab exercise 2

---
+++ user/pingpong.c
@@ -0,0 +1,29 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int
+main(int argc, char *argv[])
+{
+  int p[2];
+  if(argc != 1){
+    fprintf(2, "Usage: no extra argument...");
+    exit(1);
+  }
+
+  pipe(p);
+  if (fork() == 0){
+    read(p[0],"0", 1);
+    printf("%d: received ping\n", getpid());
+    write(p[1], "0", 1);
+    exit(0);
+  } else {
+    write(p[1], "0", 1);
+    wait(0);
+    read(p[0], "0", 1);
+    printf("%d: received pong\n", getpid());
+    exit(0);
+  }
+
+  exit(0);
+}
